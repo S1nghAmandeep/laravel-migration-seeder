a5ca702f6fa5f2961be6334bf74fc2ce68851f6a
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-12-13 18:06:04 +0100

    add condition to query

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use App\Models\Train;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -11,7 +12,9 @@ class PageController extends Controller
     public function index()
     {
 
-        $trains = Train::all();
+        // $trains = Train::all();
+        // $trains = Train::where('departure_time', '>=', Carbon::now())->get();
+        $trains = Train::where('departure_time', 'like', '2023-12-13%')->get();
         return view('layout.index', compact('trains'));
     }
 }
