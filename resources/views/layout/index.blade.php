da36a6a31905b345c931ab1f10eef1ffa0710fdf
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-12-13 18:06:52 +0100

    add risult of query to dom

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -3,10 +3,21 @@
 @section('content')
     
 <section>
-    @forelse ($trains as $train)
-        <h1>{{ $train->company }}</h1>
+   <div class="container">
+    <div class="row">
+        @forelse ($trains as $train)
+        <div class="col">
+            <h1>{{ $train->company }}</h1>
+        <h2>{{$train->departure_station}}</h2>
+        <h3>{{ $train->arrival_station }}</h3>
+        <h4>{{ $train->departure_time }}</h4>
+        <h5>{{ $train->train_code }}</h5>
+        <h6>{{ $train->price }}</h6>
+        </div>
     @empty
         
     @endforelse
+    </div>
+   </div>
 </section>
 @endsection
