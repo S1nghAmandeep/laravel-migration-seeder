0b6e2676a284084d5bc53cea31991bb41907b784
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-12-14 17:24:05 +0100

    add table

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -4,20 +4,42 @@
     
 <section>
    <div class="container">
-    <div class="row">
-        @forelse ($trains as $train)
-        <div class="col">
-            <h1>{{ $train->company }}</h1>
-        <h2>{{$train->departure_station}}</h2>
-        <h3>{{ $train->arrival_station }}</h3>
-        <h4>{{ $train->departure_time }}</h4>
-        <h5>{{ $train->train_code }}</h5>
-        <h6>{{ $train->price }}</h6>
+        <div class="row">
+            <table class="table table-primary table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col"></th>
+                        <th scope="col">Company</th>
+                        <th scope="col">Departure Station</th>
+                        <th scope="col">Arrival Station</th>
+                        <th scope="col">Departure Date/Time</th>
+                        <th scope="col">Arrival Date/Time</th>
+                        <th scope="col">Train Code</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">In time</th>
+                        <th scope="col">Canceled</th>
+                    </tr>
+                    </thead>
+                @forelse ($trains as $train)
+                    <tbody>
+                    <tr>
+                        <th scope="row"></th>
+                        <td>{{ $train->company }}</td>
+                        <td>{{ $train->departure_station }}</td>
+                        <td>{{ $train->arrival_station }}</td>
+                        <td>{{ $train->departure_time }}</td>
+                        <td>{{ $train->arrival_time }}</td>
+                        <td>{{ $train->train_code }}</td>
+                        <td>&euro; {{ $train->price }}</td>
+                        <td>{{ $train->in_time ? 'true' : 'false' }}</td>
+                        <td>{{ $train->canceled ? 'true' : 'false' }}</td>
+                    </tr>
+                @empty
+                    non ci sono risultati
+                @endforelse
+                    </tbody>
+            </table>
         </div>
-    @empty
-        
-    @endforelse
     </div>
-   </div>
 </section>
 @endsection
